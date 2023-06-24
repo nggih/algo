e8ac3fda488d54f00eb429181e4497269c663a63
Linggih Saputro <linggih.saputro@sci.ui.ac.id>
2023-06-24 13:48:58 +0700

    cpp from toki

---
+++ toki/basic/cpp_scripts/bilangan_bulat.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main() {
+
+    printf("Bilangan bulat\n");
+    printf("short , -2^15 .. 2^15 - 1, 2 byte\n");
+    printf("unsigned short , 0 .. 2^16 - 1, 2 byte\n");
+    printf("int, -2^31 .. 2^31 - 1, 4 byte\n");
+    printf("unsigned int, 0 .. 2^32 - 1, 4 byte\n");
+    printf("long long, -2^63 .. 2^63 - 1, 8 byte\n");
+    printf("unsigned long long, 0 .. 2^64 - 1, 8 byte\n");
+
+}
